6717b5a4366ab45c65d3455c94450523a8f5ef11
julihocc <juliho.castillo@outlook.com>
2024-05-21 10:36:04 -0600

    chore: Update Home component to capture and highlight input words

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 // import Paragraph from "../components/Paragraph";
 // import InputArea from "../components/InputArea";
 import { useRef, useEffect, useState } from "react";
-import { Text, TextArea } from "@radix-ui/themes";
+import { Text, TextArea, TextProps } from "@radix-ui/themes";
 
 type Color = "green" | "red" | null;
 
@@ -16,15 +16,20 @@ export default function Home() {
 
   const [colors, setColors] = useState<Color[]>([]);
 
+  const [colored, setColored] = useState<JSX.Element[] | undefined>(undefined);
+
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
 
-  const handleOnKeyUp = () => {
+  const handleOnChange = () => {
     const value = inputRef.current?.value;
     // const captured = value?.split(" ") || [];
     console.log(value);
     setCaptured(value?.split(" ") || []);
+  };
+
+  const handleOnKeyUp = () => {
     const newStatus: Color[] = captured.map((word, index) => {
       const current = words[index];
       if (current) {
@@ -39,17 +44,25 @@ export default function Home() {
   useEffect(() => {
     console.log(captured);
     console.log(colors);
+
+    if (captured) {
+      const _colored = captured.map((word, index) => {
+        const color = colors[index];
+        return <Text color={color as TextProps["color"]}>{word} </Text>;
+      });
+      setColored(_colored);
+    }
   }, [captured, colors]);
 
   return (
     <div>
       <Text>{text}</Text>
-      <TextArea ref={inputRef} onKeyUp={handleOnKeyUp} />
-      {captured &&
-        captured.map((word, index) => {
-          const color = colors[index];
-          return <Text color={color}>{word} </Text>;
-        })}
+      <TextArea
+        ref={inputRef}
+        onChange={handleOnChange}
+        onKeyUp={handleOnKeyUp}
+      />
+      {colored}
     </div>
   );
 }
