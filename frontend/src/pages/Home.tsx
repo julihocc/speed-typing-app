6bda3760d8bbf6ad7f05207fbd056675768a20a6
julihocc <juliho.castillo@outlook.com>
2024-05-21 10:10:08 -0600

    chore: Update Home component to capture and highlight input words

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,35 +3,53 @@
 import { useRef, useEffect, useState } from "react";
 import { Text, TextArea } from "@radix-ui/themes";
 
+type Color = "green" | "red" | null;
+
 export default function Home() {
   const inputRef = useRef<HTMLTextAreaElement>(null);
-  const [output, setOutput] = useState("Hola");
 
   const text = "This is a paragraph component";
+
+  const words = text.split(" ");
+
+  const [captured, setCaptured] = useState<string[]>([]);
+
+  const [colors, setColors] = useState<Color[]>([]);
 
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
 
-  const handleOnChangeTextArea = () => {
-    console.log(inputRef.current?.value);
-    setOutput(inputRef.current?.value || "");
+  const handleOnKeyUp = () => {
+    const value = inputRef.current?.value;
+    // const captured = value?.split(" ") || [];
+    console.log(value);
+    setCaptured(value?.split(" ") || []);
+    const newStatus: Color[] = captured.map((word, index) => {
+      const current = words[index];
+      if (current) {
+        return word === current ? "green" : "red";
+      } else {
+        return null;
+      }
+    });
+    setColors(newStatus);
   };
 
-  const tokens = text.split(" ");
-
-  const words = tokens.map((token, index) => (
-    <span key={index}>
-      {token}
-      {index < tokens.length - 1 && " "}
-    </span>
-  ));
+  useEffect(() => {
+    console.log(captured);
+    console.log(colors);
+  }, [captured, colors]);
 
   return (
     <div>
-      <Text>{words}</Text>
-      <TextArea ref={inputRef} onChange={handleOnChangeTextArea} />
-      <Text> {output} </Text>
+      <Text>{text}</Text>
+      <TextArea ref={inputRef} onKeyUp={handleOnKeyUp} />
+      {captured &&
+        captured.map((word, index) => {
+          const color = colors[index];
+          return <Text color={color}>{word} </Text>;
+        })}
     </div>
   );
 }
