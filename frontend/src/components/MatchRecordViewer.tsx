3a85e118699d743b77b015b0a14a7efb9a795496
julihocc <juliho.castillo@outlook.com>
2024-05-22 16:31:42 -0600

    Table for match records done

--- frontend/src/components/MatchRecordViewer.tsx
+++ frontend/src/components/MatchRecordViewer.tsx
@@ -1,22 +1,51 @@
-import  useBoundStore  from "../store";
+import useBoundStore from "../store";
+import { Table } from "@radix-ui/themes";
+
+function numberToDate(number: number | null) {
+  if (number === null) return null;
+  const gameStartDate = new Date(number);
+  const formattedDate = gameStartDate.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  });
+  return formattedDate;
+}
 
 export default function MatchRecordViewer() {
-  
   const matchRecords = useBoundStore((state) => state.getMatchRecords());
 
   return (
-    <div>
-      {matchRecords.map((matchRecord, index) => (
-        <div key={index}>
-          <p>Game start time: {matchRecord.gameStartTime}</p>
-          <p>Game end time: {matchRecord.gameEndTime}</p>
-          <p>Total words: {matchRecord.totalWords}</p>
-          <p>Nailed words: {matchRecord.nailedWords}</p>
-          <p>Total time: {matchRecord.totalTime}</p>
-          <p>Remaining time: {matchRecord.remainingTime}</p>
-          <p>Initial timer value: {matchRecord.initialTimerValue}</p>
-        </div>
-      ))}
-    </div>
+    <Table.Root>
+      <Table.Header>
+        <Table.Row>
+          <Table.Cell>Game start time</Table.Cell>
+          <Table.Cell>Total words</Table.Cell>
+          <Table.Cell>Nailed words</Table.Cell>
+          <Table.Cell>Accuracy</Table.Cell>
+          <Table.Cell>Total time</Table.Cell>
+        </Table.Row>
+        {matchRecords.map((matchRecord, index) => (
+          <Table.Row key={index}>
+            <Table.Cell>{numberToDate(matchRecord.gameStartTime)}</Table.Cell>
+            <Table.Cell>{matchRecord.totalWords}</Table.Cell>
+            <Table.Cell>{matchRecord.nailedWords}</Table.Cell>
+            <Table.Cell>
+              {matchRecord.nailedWords === null
+                ? null
+                : `${Math.round(
+                    (matchRecord.nailedWords / matchRecord.totalWords) * 100
+                  )}%`}
+            </Table.Cell>
+            <Table.Cell>
+              {matchRecord.totalTime &&
+                Math.floor(matchRecord.totalTime / 1000)}
+              s
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Header>
+    </Table.Root>
   );
 }
