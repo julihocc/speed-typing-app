e860798b4735d346ce5d2685b941ae8b575b8388
julihocc <juliho.castillo@outlook.com>
2024-05-23 16:29:02 -0600

    refactor: Update RootLayout to use Material-UI components for navigation

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,51 +1,47 @@
 import { NavLink, Outlet } from "react-router-dom";
 // import { Heading, Section, TabNav } from "@radix-ui/themes";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Container from "@mui/material/Container";
+import TimerIcon from "@mui/icons-material/Timer";
 
 export default function RootLayout() {
   return (
     <Container>
-      <Box sx={{ flexGrow: 1 }} >
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h3" sx={{ flexGrow: 1 }}>
-              Speed Typing App
-            </Typography>
-            <ButtonGroup color="secondary" variant="contained" disableElevation>
-              <Button>
-                <NavLink
-                  to="/"
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  Home
-                </NavLink>
-              </Button>
-              <Button>
-                <NavLink
-                  to="/Login"
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  Login
-                </NavLink>
-              </Button>
-              <Button>
-                <NavLink
-                  to="/Dashboard"
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  Dashboard{" "}
-                </NavLink>
-              </Button>
-            </ButtonGroup>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h3">Speed Typing App</Typography>
+          <ButtonGroup color="secondary" variant="contained" disableElevation>
+            <Button>
+              <NavLink
+                to="/"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Home
+              </NavLink>
+            </Button>
+            <Button>
+              <NavLink
+                to="/Login"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Login
+              </NavLink>
+            </Button>
+            <Button>
+              <NavLink
+                to="/Dashboard"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Dashboard
+              </NavLink>
+            </Button>
+          </ButtonGroup>
+        </Toolbar>
+      </AppBar>
       <Outlet />
     </Container>
   );
