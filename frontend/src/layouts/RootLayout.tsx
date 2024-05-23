304f8332534fec8a434bb1f70f0891a3f3b60419
julihocc <juliho.castillo@outlook.com>
2024-05-23 16:04:15 -0600

    refactor: Update RootLayout to use Material-UI components for navigation

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -4,6 +4,8 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 export default function RootLayout() {
   return (
@@ -11,28 +13,35 @@ export default function RootLayout() {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h3" sx={{ flexGrow: 1 }} >
               Speed Typing App
             </Typography>
-
-            <NavLink
-              to="/"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/Login"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              Login
-            </NavLink>
-            <NavLink
-              to="/Dashboard"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              Dashboard
-            </NavLink>
+            <ButtonGroup color="secondary" variant="contained">
+              <Button>
+                <NavLink
+                  to="/"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Home
+                </NavLink>
+              </Button>
+              <Button>
+                <NavLink
+                  to="/Login"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Login
+                </NavLink>
+              </Button>
+              <Button>
+                <NavLink
+                  to="/Dashboard"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Dashboard{" "}
+                </NavLink>
+              </Button>
+            </ButtonGroup>
           </Toolbar>
         </AppBar>
       </Box>
