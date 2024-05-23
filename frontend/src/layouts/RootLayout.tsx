9908aaa23f9f932b9a975495cd2ab895fed60f1a
julihocc <juliho.castillo@outlook.com>
2024-05-23 07:43:31 -0600

    rootlayout materialized

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,11 +1,17 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { Heading, Section, TabNav } from "@radix-ui/themes";
+// import { Heading, Section, TabNav } from "@radix-ui/themes";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function RootLayout() {
   return (
     <>
-      <Heading>Speed Typing App</Heading>
-      <TabNav.Root>
+      {/* <TabNav.Root>
         <TabNav.Link asChild>
           <NavLink to="/">Home</NavLink>
         </TabNav.Link>
@@ -15,10 +21,21 @@ export default function RootLayout() {
         <TabNav.Link asChild>
           <NavLink to="/Dashboard">Dashboard</NavLink>
         </TabNav.Link>
-      </TabNav.Root>
-      <Section>
+      </TabNav.Root> */}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Speed Typing App
+            </Typography>
+          </Toolbar>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Login">Login</NavLink>
+          <NavLink to="/Dashboard">Dashboard</NavLink>
+        </AppBar>
+
         <Outlet />
-      </Section>
+      </Box>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 import GameOverAlert from "./components/GameOverAlert";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme appearance="dark" radius="full">
+    <>
       <GameOverAlert />
       <App />
-    </Theme>
+    </>
   </React.StrictMode>
 );
