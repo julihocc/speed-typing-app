6881eb3272e117573bf82a3e424a020cb81a172f
julihocc <juliho.castillo@outlook.com>
2024-05-22 18:11:55 -0600

    Line chart not working

---
+++ frontend/src/components/LineChartHero.tsx
@@ -0,0 +1,80 @@
+import { LineChart } from "./LineChart";
+
+const chartdata = [
+  {
+    date: "Jan 23",
+    SolarPanels: 2890,
+    Inverters: 2338,
+  },
+  {
+    date: "Feb 23",
+    SolarPanels: 2756,
+    Inverters: 2103,
+  },
+  {
+    date: "Mar 23",
+    SolarPanels: 3322,
+    Inverters: 2194,
+  },
+  {
+    date: "Apr 23",
+    SolarPanels: 3470,
+    Inverters: 2108,
+  },
+  {
+    date: "May 23",
+    SolarPanels: 3475,
+    Inverters: 1812,
+  },
+  {
+    date: "Jun 23",
+    SolarPanels: 3129,
+    Inverters: 1726,
+  },
+  {
+    date: "Jul 23",
+    SolarPanels: 3490,
+    Inverters: 1982,
+  },
+  {
+    date: "Aug 23",
+    SolarPanels: 2903,
+    Inverters: 2012,
+  },
+  {
+    date: "Sep 23",
+    SolarPanels: 2643,
+    Inverters: 2342,
+  },
+  {
+    date: "Oct 23",
+    SolarPanels: 2837,
+    Inverters: 2473,
+  },
+  {
+    date: "Nov 23",
+    SolarPanels: 2954,
+    Inverters: 3848,
+  },
+  {
+    date: "Dec 23",
+    SolarPanels: 3239,
+    Inverters: 3736,
+  },
+];
+
+export const LineChartHero = () => (
+  <>
+    hi
+    <LineChart
+      className="h-80"
+      data={chartdata}
+      index="date"
+      categories={["SolarPanels", "Inverters"]}
+      valueFormatter={(number: number) =>
+        `$${Intl.NumberFormat("us").format(number).toString()}`
+      }
+      onValueChange={(v) => console.log(v)}
+    />
+  </>
+);
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,18 @@
 import MatchRecordViewer from "../components/MatchRecordViewer";
 import ResetMatchRecords from "../components/ResetMatchRecords";
+import { LineChartHero } from "../components/LineChartHero";
+import { Box } from "@radix-ui/themes";
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <ResetMatchRecords />
-      <MatchRecordViewer />
-    </div>
+    // <div>
+    //   <h1>Dashboard</h1>
+    //   <Box>
+    //     <LineChartHero />
+    //   </Box>
+    //   <ResetMatchRecords />
+    //   <MatchRecordViewer />
+    // </div>\
+      <LineChartHero />
   );
 }
