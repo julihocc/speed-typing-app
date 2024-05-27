3dbf0ea519478ed8bdb36e14dc6f1cff77d9e541
julihocc <juliho.castillo@outlook.com>
2024-05-27 13:48:30 -0600

    feat: Add currentUserEmail check in Dashboard component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,8 +3,13 @@ import ResetMatchRecords from "../components/ResetMatchRecords";
 import HistoricalMatchAccuracy from "../components/HistoricalMatchAccuracy";
 import SpeedWatcher from "../components/SpeedWatcher";
 import PageLayout from "../layouts/PageLayout";
+import useBoundStore from "../stores/bound-store";
 
 export default function Dashboard() {
+  const { currentUserEmail } = useBoundStore();
+  if (currentUserEmail===null) {
+    return null;
+  }
   return (
     <PageLayout title="Dashboard">
       <SpeedWatcher />
