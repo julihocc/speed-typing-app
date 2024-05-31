954619e457d6006e4e90001d58efa21e4c07dce2
julihocc <juliho.castillo@outlook.com>
2024-05-31 11:44:33 -0600

    refactor: Remove GameOverAlert component and related code

--- frontend/src/stores/session-store/gameSlice.tsx
+++ frontend/src/stores/session-store/gameSlice.tsx
@@ -27,65 +27,119 @@ export const createGameSlice: StateCreator<
   gameMode: "1",
   backspaceDisabled: false,
   setTextFieldValue: (text) =>
-    set((state) => {
-      state.textFieldValue = text;
-      return state;
-    }, false, "setTextFieldValue"),
+    set(
+      (state) => {
+        state.textFieldValue = text;
+        return state;
+      },
+      false,
+      "setTextFieldValue"
+    ),
   setCaptured: (captured) =>
-    set((state) => {
-      state.captured = captured;
-      return state;
-    }, false, "setCaptured"),
+    set(
+      (state) => {
+        state.captured = captured;
+        return state;
+      },
+      false,
+      "setCaptured"
+    ),
   setNailed: (nailed) =>
-    set((state) => {
-      state.nailed = nailed;
-      return state;
-    }, false, "setNailed"),
+    set(
+      (state) => {
+        state.nailed = nailed;
+        return state;
+      },
+      false,
+      "setNailed"
+    ),
   setColors: (colors) =>
-    set((state) => {
-      state.colors = colors;
-      return state;
-    }, false, "setColors"),
+    set(
+      (state) => {
+        state.colors = colors;
+        return state;
+      },
+      false,
+      "setColors"
+    ),
   setGameStartTime: (initTime) =>
-    set((state) => {
-      state.gameStartTime = initTime;
-      return state;
-    }, false, "setGameStartTime"),
+    set(
+      (state) => {
+        state.gameStartTime = initTime;
+        return state;
+      },
+      false,
+      "setGameStartTime"
+    ),
   setGameEndTime: (endTime) =>
-    set((state) => {
-      state.gameEndTime = endTime;
-      return state;
-    }, false, "setGameEndTime"),
+    set(
+      (state) => {
+        state.gameEndTime = endTime;
+        return state;
+      },
+      false,
+      "setGameEndTime"
+    ),
   resetGame: () =>
-    set((state) => {
-      // state.words = [];
-      state.captured = [];
-      state.nailed = [];
-      state.colors = [];
-      state.gameStartTime = null;
-      state.gameEndTime = null;
-      state.textFieldValue = undefined;
-      state.randomIndex = null;
-      return state;
-    }, false, "resetGame"),
+    set(
+      (state) => {
+        // chars: [],
+        // captured: [],
+        // nailed: [],
+        // colors: [],
+        // gameStartTime: null,
+        // gameEndTime: null,
+        // textFieldValue: undefined,
+        // randomIndex: null,
+        // gameMode: "1",
+        // backspaceDisabled: false,
+        state.chars = [];
+        state.captured = [];
+        state.nailed = [];
+        state.colors = [];
+        state.gameStartTime = null;
+        state.gameEndTime = null;
+        state.textFieldValue = undefined;
+        state.randomIndex = null;
+        return state;
+      },
+      false,
+      "resetGame"
+    ),
   setChars: (text) =>
-    set((state) => {
-      state.chars = text;
-      return state;
-    }, false, "setChars"),
+    set(
+      (state) => {
+        state.chars = text;
+        return state;
+      },
+      false,
+      "setChars"
+    ),
   setRandomIndex: (randomIndex) =>
-    set((state) => {
-      state.randomIndex = randomIndex;
-      return state;
-    }, false, "setRandomIndex"),
+    set(
+      (state) => {
+        state.randomIndex = randomIndex;
+        return state;
+      },
+      false,
+      "setRandomIndex"
+    ),
   setGameMode: (gameMode) =>
-    set((state) => {
-      state.gameMode = gameMode;
-      return state;
-    }, false, "setGameMode"),
+    set(
+      (state) => {
+        state.gameMode = gameMode;
+        return state;
+      },
+      false,
+      "setGameMode"
+    ),
   setBackspaceDisabled: (backspaceDisabled) =>
-    set((state) => {
-      state.backspaceDisabled = backspaceDisabled;
-      return state;
-    }, false, "setBackspaceDisabled"),
+    set(
+      (state) => {
+        state.backspaceDisabled = backspaceDisabled;
+        return state;
+      },
+      false,
+      "setBackspaceDisabled"
+    ),
 });
