76ce511e88dbb23850969a2e758f77b7e3120cf0
julihocc <juliho.castillo@outlook.com>
2024-05-31 16:32:12 -0600

    refactor: Update Timer component to optimize animation and remove unused code

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -1,12 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import useSessionStore from "../stores/session-store";
 import Box from "@mui/material/Box";
-import { LinearProgress, Typography } from "@mui/material";
-import { Gauge } from "@mui/x-charts/Gauge";
+import { Typography } from "@mui/material";
+
 import { useSpring, animated } from "@react-spring/web";
 
 export default function Timer() {
-  // const [remainingTime, setRemainingTime] = useState<number>(60);
   const initialTimerValue = useSessionStore((state) => state.initialTimerValue);
   const remainingTime = useSessionStore((state) => state.remainingTime);
   const setRemainingTime = useSessionStore((state) => state.setRemainingTime);
@@ -35,8 +34,6 @@ export default function Timer() {
     return () => clearInterval(timer);
   }, [remainingTime, setRemainingTime, gameEndTime]);
 
-  // Animation configuration
-
   const [secUnits, setSecUnits] = useState<number | null>(null);
   const [secTens, setSecTens] = useState<number | null>(null);
   const prevSecUnitsRef = useRef(secUnits);
@@ -46,14 +43,12 @@ export default function Timer() {
     from: { y: -20, opacity: 0 },
     to: { y: 0, opacity: 1 },
     config: { duration: 1000 },
-    // reset: true,
   });
 
   const animationSecTens = useSpring({
     from: { y: -20, opacity: 0 },
     to: { y: 0, opacity: 1 },
     config: { duration: 1000 },
-    // reset: true,
   });
 
   useEffect(() => {
@@ -72,47 +67,29 @@ export default function Timer() {
     if (remainingTime !== null) {
       const newSecTens = Math.floor(remainingTime / 10);
       setSecTens(newSecTens);
-      console.log(`newSecTens: ${newSecTens}`);
-      console.log(`prevSecTensRef.current: ${prevSecTensRef.current}`);
+
       if (prevSecTensRef.current !== newSecTens) {
-        console.log("Starting animation");
         animationSecTens.y.reset();
         animationSecTens.opacity.reset();
-        // prevSecTensRef.current = newSecTens;
       }
       prevSecTensRef.current = newSecTens;
     }
   }, [animationSecTens, remainingTime]);
 
-  // useEffect(() => {
-  //   console.log(`remainingTime: ${remainingTime}`);
-  //   if (remainingTime !== null) {
-  //     setSecUnits(remainingTime % 10);
-  //   }
-  // }, [remainingTime]);
-
-  // useEffect(() => {
-
-  //   console.log(`remainingTime: ${remainingTime}`);
-  //   console.log(prevSecTensRef.current);
-  //   console.log(secTens);
-  //   if (remainingTime !== null) {
-  //     setSecTens(Math.floor(remainingTime / 10));
-  //   }
-  // }, [remainingTime, secTens]);
-
   return (
     <div>
-      <Box display="flex" alignItems="center" gap={4} margin={2}>
+      <Box display="flex" alignItems="center" margin={2}>
         {" "}
-        <AnimatedTypography variant="h2" style={animationSecTens}>
+        <Typography>Remaining time:</Typography>
+        <AnimatedTypography style={animationSecTens}>
           {remainingTime === null
             ? Math.floor(initialTimerValue / 10)
             : secTens}
         </AnimatedTypography>
-        <AnimatedTypography variant="h2" style={animationSecUnits}>
+        <AnimatedTypography style={animationSecUnits}>
           {remainingTime === null ? initialTimerValue % 10 : secUnits}
         </AnimatedTypography>
+        <Typography>seconds.</Typography>
       </Box>
     </div>
   );
